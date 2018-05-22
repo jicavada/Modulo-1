6749f76297d9267f791b9b9330579e819d9f79b7
Juan <frekuenze@hotmail.com>
2018-05-22 11:48:56 +0200

    final

---
+++ ejercicio2/ejercicio2.py
@@ -0,0 +1,37 @@
+
+'''
+IMF Big Data Master ejercicio 2 modulo 1
+Juan Iglesias Cavada
+
+
+'''
+def get_sentimientos(archivo):
+    '''
+    lee el archivo de términos y devuelve un diccionario creado con los mismos
+    '''
+    with open(archivo, 'r') as sentimientos:
+        valores = {}
+        for linea in sentimientos:
+            termino,valor = linea.split('\t')
+            valores[termino] = int(valor)
+    return valores
+
+
+valores = get_sentimientos("Sentimientos.txt")
+with open('tweets.txt', 'r') as tweets:
+    for tweet in tweets:
+        if len(tweet) < 2: #excluye tweets que tengan menos de 2 caracteres, siendo uno \n
+            continue
+        print(tweet)
+        words_not_rated = []
+        sentiment = 0
+        for word in tweet.split(): #para cada palabra ver si existe en el diccionario y actualiza su sentimiento
+            if word in valores:
+                sentiment += valores[word]
+            else:
+                words_not_rated.append(word)
+        for word in words_not_rated:
+            print(word + ':' + str(sentiment))
+
+        
+
