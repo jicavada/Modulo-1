f4d60f46672bbabffcb87d2dd80d27f7715acb0d
Juan <frekuenze@hotmail.com>
2018-05-22 11:52:00 +0200

    final

--- ejercicio2/ejercicio2.py
+++ ejercicio2/ejercicio2.py
@@ -3,6 +3,9 @@
 IMF Big Data Master ejercicio 2 modulo 1
 Juan Iglesias Cavada
 
+Este programa lee tweets del archivo tweets.txt y encuentra las palabras que no tienen equivalencia en
+el diccionario sentimientos.txt. Posteriormente les asigna el valor del sentimiento del tweet en el que
+se encuentran y las muestra.
 
 '''
 def get_sentimientos(archivo):
@@ -22,7 +25,7 @@ with open('tweets.txt', 'r') as tweets:
     for tweet in tweets:
         if len(tweet) < 2: #excluye tweets que tengan menos de 2 caracteres, siendo uno \n
             continue
-        print(tweet)
+        print('\n' + tweet)
         words_not_rated = []
         sentiment = 0
         for word in tweet.split(): #para cada palabra ver si existe en el diccionario y actualiza su sentimiento
